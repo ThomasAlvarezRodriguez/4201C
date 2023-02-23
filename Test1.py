f97a18abed0621da282ea4c67743ad60b2c231f8
BernardVonBretzel <bernard@vonbretzel.fr>
2023-02-23 14:17:24 +0100

    mongo

---
+++ Test1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pymongo import MongoClient
+
+# Load CSV file into pandas dataframe
+df = pd.read_csv('DataTraité.csv', sep=';')
+
+# Connect to MongoDB database
+client = MongoClient('mongodb://localhost:27017/')
+db = client['4201C']
+
+# Drop collection if it already exists
+db['SSD-HDD'].drop()
+
+# Insert data into MongoDB collection
+db['SSD-HDD'].insert_many(df.to_dict('records'))
+
+# Test if data was inserted correctly
+print(db['SSD-HDD'].find_one())
+
