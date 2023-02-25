69bd7199e4ec1ed217485b5f8c4f2608efb3b2f1
Alexandre Dias <alexandre.dias@edu.esiee.fr>
2023-02-25 23:59:30 +0100

    readme commencé + app finie

--- amazon/spiders/amazon_search.py
+++ amazon/spiders/amazon_search.py
@@ -1,31 +1,31 @@
 import scrapy
 from urllib.parse import urljoin
 
-class AmazonSearchSpider(scrapy.Spider):
-    name = "amazon_search"
+class AmazonSearchSpider(scrapy.Spider): 
+    name = "amazon_search" # Nom du spider, il est utilisé pour lancer le spider avec la commande: scrapy crawl amazon_search
 
-    custom_settings = {
+    custom_settings = { # Paramètres custom pour le spider, dans notre cas on va utiliser le pipeline pour exporter les données dans un fichier csv
         'FEEDS': { 'data/%(name)s_%(time)s.csv': { 'format': 'csv',}}
         }
 
     def start_requests(self):
-        keyword_list = ['SSD','HDD']
-        for keyword in keyword_list:
-            amazon_search_url = f'https://www.amazon.fr/s?k={keyword}&page=1'
-            yield scrapy.Request(url=amazon_search_url, callback=self.parse_search_results, meta={'keyword': keyword, 'page': 1})
+        keyword_list = ['SSD','HDD'] # Liste des mots clés à rechercher sur Amazon 
+        for keyword in keyword_list: # Pour chaque mot clé on va générer une requête pour récupérer les résultats de la recherche
+            amazon_search_url = f'https://www.amazon.fr/s?k={keyword}&page=1' # Url de la recherche
+            yield scrapy.Request(url=amazon_search_url, callback=self.parse_search_results, meta={'keyword': keyword, 'page': 1}) # On envoie la requête avec le callback qui va parser les résultats de la recherche
 
-    def parse_search_results(self, response):
-        page = response.meta['page']
-        keyword = response.meta['keyword'] 
+    def parse_search_results(self, response): # Fonction qui va parser les résultats de la recherche
+        page = response.meta['page'] # On récupère la page courante
+        keyword = response.meta['keyword']  # On récupère le mot clé de la recherche
 
         ## Extract Overview Product Data
-        search_products = response.css("div.s-result-item[data-component-type=s-search-result]")
-        for product in search_products:
-            relative_url = product.css("h2>a::attr(href)").get()
-            asin = relative_url.split('/')[3] if len(relative_url.split('/')) >= 4 else None
-            product_url = urljoin('https://www.amazon.fr/', relative_url).split("?")[0]
-            yield  {
-                    "keyword": keyword,
+        search_products = response.css("div.s-result-item[data-component-type=s-search-result]") # On récupère les produits de la page courante
+        for product in search_products: # Pour chaque produit on va extraire les données
+            relative_url = product.css("h2>a::attr(href)").get() # On récupère l'url relative du produit
+            asin = relative_url.split('/')[3] if len(relative_url.split('/')) >= 4 else None # On récupère l'asin du produit
+            product_url = urljoin('https://www.amazon.fr/', relative_url).split("?")[0] # On récupère l'url absolue du produit
+            yield  { # On retourne un dictionnaire avec les données extraites
+                    "keyword": keyword, 
                     "asin": asin,
                     "url": product_url,
                     "ad": True if "/slredirect/" in product_url else False, 
@@ -40,14 +40,14 @@ class AmazonSearchSpider(scrapy.Spider):
 
 
         ## Get All Pages
-        if page == 1:
-            available_pages = response.xpath(
-                '//a[has-class("s-pagination-item")][not(has-class("s-pagination-separator"))]/text()'
-            ).getall()
+        if page == 1: 
+            available_pages = response.xpath(  # On récupère les pages disponibles
+                '//a[has-class("s-pagination-item")][not(has-class("s-pagination-separator"))]/text()' 
+            ).getall() 
 
-            for page_num in available_pages:
-                amazon_search_url = f'https://www.amazon.fr/s?k={keyword}&page={page_num}'
-                yield scrapy.Request(url=amazon_search_url, callback=self.parse_search_results, meta={'keyword': keyword, 'page': page_num})
+            for page_num in available_pages: # Pour chaque page on va générer une requête pour récupérer les résultats de la recherche
+                amazon_search_url = f'https://www.amazon.fr/s?k={keyword}&page={page_num}' # Url de la recherche
+                yield scrapy.Request(url=amazon_search_url, callback=self.parse_search_results, meta={'keyword': keyword, 'page': page_num}) # On envoie la requête avec le callback qui va parser les résultats de la recherche
 
 
         
--- amazon/spiders/amazon_search_product.py
+++ amazon/spiders/amazon_search_product.py
@@ -3,29 +3,28 @@ import scrapy
 from urllib.parse import urljoin
 import re
 
-class AmazonSearchProductSpider(scrapy.Spider):
-    name = "SSD-HDD"
-
-    custom_settings = {
+class AmazonSearchProductSpider(scrapy.Spider): 
+    name = "SSD-HDD" # Nom du spider, il est utilisé pour lancer le spider avec la commande: scrapy crawl SSD-HDD
+    custom_settings = { # Paramètres custom pour le spider, dans notre cas on va utiliser le pipeline pour exporter les données dans un fichier csv
         'FEEDS': { 'data/%(name)s_%(time)s.csv': { 'format': 'csv',}}
         }
 
-    def start_requests(self):
-        keyword_list = ['SSD','HDD']
-        for keyword in keyword_list:
-            amazon_search_url = f'https://www.amazon.fr/s?k={keyword}&page=1'
-            yield scrapy.Request(url=amazon_search_url, callback=self.discover_product_urls, meta={'keyword': keyword, 'page': 1})
+    def start_requests(self): # Fonction qui va générer les requêtes pour récupérer les résultats de la recherche
+        keyword_list = ['SSD','HDD'] # Liste des mots clés à rechercher sur Amazon
+        for keyword in keyword_list: # Pour chaque mot clé on va générer une requête pour récupérer les résultats de la recherche
+            amazon_search_url = f'https://www.amazon.fr/s?k={keyword}&page=1' # On génère l'url de la recherche en fonction du mot clé
+            yield scrapy.Request(url=amazon_search_url, callback=self.discover_product_urls, meta={'keyword': keyword, 'page': 1}) # On génère la requête et on passe le mot clé et le numéro de page en paramètre
 
-    def discover_product_urls(self, response):
-        page = response.meta['page']
-        keyword = response.meta['keyword'] 
+    def discover_product_urls(self, response): # Fonction qui va récupérer les urls des produits
+        page = response.meta['page'] # On récupère le numéro de page
+        keyword = response.meta['keyword']  # On récupère le mot clé
 
         ## Discover Product URLs
-        search_products = response.css("div.s-result-item[data-component-type=s-search-result]")
-        for product in search_products:
+        search_products = response.css("div.s-result-item[data-component-type=s-search-result]") # On récupère les produits de la page
+        for product in search_products: # Pour chaque produit on va récupérer l'url du produit
             relative_url = product.css("h2>a::attr(href)").get()
             product_url = urljoin('https://www.amazon.fr/', relative_url).split("?")[0]
-            yield scrapy.Request(url=product_url, callback=self.parse_product_data, meta={'keyword': keyword, 'page': page})
+            yield scrapy.Request(url=product_url, callback=self.parse_product_data, meta={'keyword': keyword, 'page': page}) # On génère la requête et on passe le mot clé et le numéro de page en paramètre
             
         ## Get All Pages
         if page == 1:
@@ -38,12 +37,12 @@ class AmazonSearchProductSpider(scrapy.Spider):
                 yield scrapy.Request(url=amazon_search_url, callback=self.discover_product_urls, meta={'keyword': keyword, 'page': page_num})
 
 
-    def parse_product_data(self, response):
+    def parse_product_data(self, response): # Fonction qui va récupérer les données du produit
         keyword = response.meta['keyword']
         price = response.css('.a-price span[aria-hidden="true"] ::text').get("")
-        if not price:
-            price = response.css('.a-price .a-offscreen ::text').get("")
-        yield {
+        if not price: # Si le prix n'est pas trouvé on va chercher le prix dans un autre élément
+            price = response.css('.a-price .a-offscreen ::text').get("") 
+        yield { # On retourne les données du produit
             "name": response.css("#productTitle::text").get("").strip(),
             "asin": response.css("input#ASIN::attr(value)").get(),
             "price": price,
